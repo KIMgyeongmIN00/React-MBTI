4cd0a65e35bd35134aa98181a01ef428c142a6c3
KGyeong <rlarudals61@naver.com>
2025-02-21 22:01:29 +0900

    feat:json-server-api-1차연결

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,3 +1,49 @@
+import {
+  getTestResults,
+  getFilterdResults,
+  createTestResult,
+  deleteTestResult,
+  updateTestResultVisibility,
+} from "../utils/jsonAPI";
+
 export default function Profile() {
-  return <div>Profile</div>;
+  async function handleCallResults() {
+    const response = await getTestResults("resultsTable");
+    console.log(response);
+    const filterdResponse = await getFilterdResults(
+      "resultsTable",
+      "views",
+      300
+    );
+    console.log(filterdResponse);
+    const create = await createTestResult("resultsTable", {
+      id: "11",
+      title: "내가 임의적으로 만든거",
+      views: 333,
+      test: "ehlsk",
+    });
+    console.log(create);
+  }
+
+  async function handleDeleteResults() {
+    const deleteResult = await deleteTestResult("resultsTable", "11");
+    console.log(deleteResult);
+  }
+
+  async function handleUpdateResults() {
+    const update = await updateTestResultVisibility("resultsTable", "11", {
+      title: "간단한 수정",
+    });
+    console.log(update);
+  }
+
+  return (
+    <div>
+      <button onClick={handleCallResults}>불러와봐</button>
+      <br />
+      <button onClick={handleDeleteResults}>삭제한다</button>
+      <br />
+      <button onClick={handleUpdateResults}>변경한다</button>
+    </div>
+  );
 }
